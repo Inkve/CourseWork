cda2f17a0d9c44da6e3e1272739018d431003b1d
Inkve <levin.andrei.2003@gmail.com>
2022-04-17 18:12:47 +0300

    класс треугольника

---
+++ CourseWork/Figure.h
@@ -0,0 +1,7 @@
+#pragma once
+using namespace std;
+
+class Figure {
+public:
+	virtual void show() = 0;
+};
---
+++ CourseWork/Triangle.cpp
@@ -0,0 +1,11 @@
+#include "Triangle.h"
+
+triangle::triangle(int x_, int y_, int a_) {
+	x = x_;
+	y = y_;
+	a = a_;
+}
+
+void triangle::show() {
+
+};
